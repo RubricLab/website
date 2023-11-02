432b456f3bf5ff0410e760873bbf30a0d8bab101
DexterStorey <36115192+DexterStorey@users.noreply.github.com>
2023-11-01 20:29:21 -0400

    update Announcement.tsx

--- app/components/Announcement.tsx
+++ app/components/Announcement.tsx
@@ -5,9 +5,9 @@ export default function Announcement() {
 	return (
 		<Link
 			className='group flex items-center gap-3 rounded-full border-2 border-neutral-500 bg-neutral-200 px-3.5 py-2 text-black no-underline shadow-lg dark:bg-neutral-950 dark:text-white'
-			href='https://cal.ai'>
+			href='https://twitter.com/RubricLabs/status/1719812696575529220'>
 			<p className='text-sm sm:text-base'>
-				<span className='font-semibold'>New:</span> Check out our launch with Cal.ai
+				<span className='font-semibold'>New:</span> Check out our launch of create-rubric-app
 			</p>
 			<ArrowUpRight className='transition-all duration-300 group-hover:-translate-y-1 group-hover:translate-x-1' />
 		</Link>
