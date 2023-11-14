dcc4be32f301a1825ed7553be9646c24508f9c14
Sarim Malik <sarim@rubriclabs.com>
2023-11-14 15:18:31 -0500

    Fix padding

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -2,7 +2,7 @@ import NewsletterForm from '../components/NewsletterForm'
 
 export default function Newsletter() {
 	return (
-		<main className='flex min-h-screen items-center justify-center'>
+		<main className='flex min-h-screen items-center justify-center px-5 sm:px-10'>
 			<div className='flex w-full max-w-sm flex-col gap-8'>
 				<div className='flex flex-col'>
 					<h1>The Grid</h1>
