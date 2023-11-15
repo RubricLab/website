cd4ba24ccebd4308bc920c5589518cc496e7e936
Sarim Malik <sarim@rubriclabs.com>
2023-11-14 22:24:44 -0500

    Hide arrow on Card by default

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -15,10 +15,12 @@ export const Card = ({url, title, body}: CardProps) => {
 			className='group relative w-full rounded-xl border-2 border-neutral-400 bg-white p-10 no-underline dark:border-neutral-700 dark:bg-black'
 			href={url}
 			target='_blank'>
-			<div className='flex flex-col items-start gap-3 sm:flex-row sm:items-center sm:gap-5'>
-				<h1 className='text-primary flex-wrap text-4xl sm:w-2/5'>{title}</h1>
+			<div className='flex h-full flex-col items-start gap-3 sm:flex-row sm:items-center sm:gap-5'>
+				<h1 className='text-primary flex h-full flex-wrap items-center text-4xl sm:w-2/5'>
+					{title}
+				</h1>
 				{body && (
-					<div className='sm:w-4/5'>
+					<div className='flex h-full items-center sm:w-4/5'>
 						{typeof body === 'string' ? (
 							<p className='text-secondary text-lg'>{body}</p>
 						) : (
@@ -27,7 +29,7 @@ export const Card = ({url, title, body}: CardProps) => {
 					</div>
 				)}
 			</div>
-			<ArrowUpRightIcon className='text-tertiary group-hover:text-secondary absolute right-4 top-4 h-8 w-8 transition-all duration-300 group-hover:-translate-y-1 group-hover:translate-x-1' />
+			<ArrowUpRightIcon className='text-tertiary group-hover:text-secondary absolute right-4 top-4 h-8 w-8 opacity-0 transition-all duration-300 group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:opacity-100' />
 		</Link>
 	)
 }
