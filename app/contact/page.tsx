a7a8e6be588246006badfe68234385cd32762808
Sarim Malik <sarim@rubriclabs.com>
2023-11-15 22:25:39 -0500

    Add metadata for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,19 @@
+import {Metadata} from 'next'
 import ContactForm from '~/components/ContactForm'
+import {DEFAULT_META, META} from '~/constants/metadata'
+
+export const metadata: Metadata = {
+	...DEFAULT_META,
+	openGraph: {
+		...DEFAULT_META.openGraph,
+		title: `Contact | ${META.title}`
+	},
+	title: `Contact | ${META.title}`,
+	twitter: {
+		...DEFAULT_META.twitter,
+		title: `Contact | ${META.title}`
+	}
+}
 
 export default function Contact() {
 	return (
