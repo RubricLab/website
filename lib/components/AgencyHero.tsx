16fd3798a6319c78056936ceb87dc572e4234c2f
tedspare <ted.spare@gmail.com>
2024-01-10 12:33:18 -0500

    Adjust hero

--- lib/components/AgencyHero.tsx
+++ lib/components/AgencyHero.tsx
@@ -1,12 +1,10 @@
 'use client'
 import {AnimatePresence, motion} from 'framer-motion'
-import {useRouter} from 'next/navigation'
+import Link from 'next/link'
 
 export default function Hero() {
-	const router = useRouter()
-
 	return (
-		<div className='flex min-h-screen flex-col items-center justify-center gap-5 p-8'>
+		<div className='flex min-h-screen flex-col items-center justify-center gap-5 p-5 pt-20 sm:px-10'>
 			<AnimatePresence>
 				<motion.div
 					animate={{opacity: 1}}
@@ -14,18 +12,25 @@ export default function Hero() {
 					exit={{opacity: 0}}
 					initial={{opacity: 0}}
 					transition={{duration: 2}}>
-					<div className='flex flex-col gap-16 text-black dark:text-white'>
-						<p className='text-[100px]'>
-							We&apos;re a digital agency making <b>AI-first</b> software.
+					<div className='flex h-full flex-col gap-8 text-black dark:text-white sm:gap-16'>
+						<p className='text-6xl tracking-tight sm:text-8xl sm:leading-[7.5rem]'>
+							We make <span className='whitespace-nowrap font-bold'>AI-enabled</span>,{' '}
+							<span className='font-bold'>magical</span> software.
+						</p>
+						<p className='text-secondary max-w-xl text-2xl sm:leading-10'>
+							Rubric is a digital studio working with startups big & small to build the
+							future users want.
 						</p>
 						<p className='text-secondary invisible sm:visible'>
 							Press{' '}
-							<span
-								className='mx-1 cursor-pointer rounded-md bg-black px-2.5 py-1.5 font-medium text-white transition-opacity hover:opacity-80 dark:bg-white dark:text-black'
-								onClick={() => router.push('/contact')}>
-								R
-							</span>{' '}
-							anytime to get started
+							<Link
+								href='/contact'
+								className='no-underline'>
+								<span className='mx-0.5 cursor-pointer rounded-md bg-black px-2 py-1 font-medium text-white transition-opacity hover:opacity-80 dark:bg-white dark:text-black'>
+									R
+								</span>
+							</Link>{' '}
+							anytime to get started.
 						</p>
 					</div>
 				</motion.div>
