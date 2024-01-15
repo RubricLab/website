24a15e9748226a5b9be43c43a5405ab17bb89ae0
tedspare <ted.spare@gmail.com>
2024-01-15 17:38:41 -0500

    Fix layout shift and hover-jump on FAQ

--- lib/components/FAQs.tsx
+++ lib/components/FAQs.tsx
@@ -1,18 +1,17 @@
 'use client'
 import {ChevronDownIcon} from 'lucide-react'
 import {useState} from 'react'
-import Button from './Button'
 
 const items = [
 	{
 		question: 'What is your project minimum?',
 		answer: (
 			<>
-				<p>$20k.</p>
-				<p>
+				<div>$20k.</div>
+				<div>
 					However, it depends on the project and team. We make exceptions depending
 					on great founder-team fit, so feel free to reach out.
-				</p>
+				</div>
 			</>
 		)
 	},
@@ -20,11 +19,11 @@ const items = [
 		question: 'Do I work with your engineers directly?',
 		answer: (
 			<>
-				<p>No.</p>
-				<p>
+				<div>No.</div>
+				<div>
 					You interact with a member of our team at a weekly or biweekly cadence with
 					project updates.
-				</p>
+				</div>
 			</>
 		)
 	},
@@ -32,8 +31,8 @@ const items = [
 		question: 'Do you help with maintenance after the project?',
 		answer: (
 			<>
-				<p>Yes.</p>
-				<p>Maintenance is paid and negotiated in the contract upfront.</p>
+				<div>Yes.</div>
+				<div>Maintenance is paid and negotiated in the contract upfront.</div>
 			</>
 		)
 	}
@@ -44,11 +43,10 @@ function FAQ({question, answer}) {
 
 	return (
 		<div
-			className={`flex h-12 w-full cursor-pointer flex-col gap-3 rounded-md bg-neutral-100 p-3 transition-[height] duration-200 ease-in hover:h-36 dark:bg-neutral-950 ${
-				expand ? 'justify-start' : 'justify-center'
+			className={`flex w-full cursor-pointer flex-col justify-start gap-3 rounded-md bg-neutral-100 p-3 transition-[height] duration-200 ease-in dark:bg-neutral-950 ${
+				expand ? 'h-36' : 'h-12'
 			}`}
-			onMouseEnter={() => setExpand(true)}
-			onMouseLeave={() => setExpand(false)}>
+			onClick={() => setExpand(val => !val)}>
 			<div className='flex w-full items-center justify-between'>
 				<p className='font-bold'>{question}</p>
 				<ChevronDownIcon
@@ -57,7 +55,12 @@ function FAQ({question, answer}) {
 					} h-5 w-5 transition-transform duration-300`}
 				/>
 			</div>
-			{expand && <div className='flex flex-col gap-2'>{answer}</div>}
+			<div
+				className={`text-tertiary flex flex-col gap-2 transition-opacity ${
+					expand ? 'opacity-100' : 'pointer-events-none opacity-0'
+				}`}>
+				{answer}
+			</div>
 		</div>
 	)
 }
@@ -66,9 +69,9 @@ export default function FAQs() {
 	return (
 		<div
 			id='faq'
-			className='flex min-h-screen flex-col items-center justify-center gap-16 bg-white p-8 dark:bg-black'>
+			className='flex min-h-screen flex-col items-center justify-start gap-16 bg-white p-8 dark:bg-black sm:py-28'>
 			<h1>Frequently asked questions</h1>
-			<div className='flex w-full max-w-3xl flex-col gap-3'>
+			<div className='flex h-96 w-full max-w-3xl grow flex-col gap-3'>
 				{items.map(item => (
 					<FAQ
 						key={item.question}
@@ -77,12 +80,6 @@ export default function FAQs() {
 					/>
 				))}
 			</div>
-			<Button
-				body='Get in touch'
-				className='w-fit'
-				href='/contact'
-				variant='dark'
-			/>
 		</div>
 	)
 }
