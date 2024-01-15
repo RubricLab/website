e5b3c504633087d93e3e93e55fadd157a9799aea
tedspare <ted.spare@gmail.com>
2024-01-15 17:52:43 -0500

    Tweak quote

--- lib/components/CTA.tsx
+++ lib/components/CTA.tsx
@@ -6,8 +6,8 @@ export const CTA = () => {
 			id='cta'
 			className='flex min-h-screen w-screen flex-col items-center justify-center gap-10 bg-neutral-100 p-8 text-center dark:bg-neutral-950'>
 			<h1 className='text-secondary max-w-2xl italic leading-[3.5rem]'>
-				the people who are crazy enough to think they can change the world are the
-				ones who do
+				those who are crazy enough to think they can change the world are the ones
+				who do
 			</h1>
 			<h4>Now accepting projects for 2Q24.</h4>
 			<Button
