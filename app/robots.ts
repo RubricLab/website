fa068caaa0a8019606a3ab9846c66592c56d996b
DexterStorey <dexter@dexterstorey.com>
2024-02-22 00:01:15 -0500

    add back robots no scrape (again)

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,12 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			allow: '/',
-			disallow: ['/_next/static/media/', '*/icon', '*/apple-icon'],
+			disallow: [
+				'/_next/static/media/',
+				'*/opengraph-image',
+				'*/icon',
+				'*/apple-icon'
+			],
 			userAgent: '*'
 		},
 		sitemap: 'https://rubriclabs.com/sitemap.xml'
