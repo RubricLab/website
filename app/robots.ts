d9f526e92610e5fc64e329600c3710c37776f515
DexterStorey <dexter@dexterstorey.com>
2024-02-21 22:41:31 -0500

    allow crawling of opengraph-image

--- app/robots.ts
+++ app/robots.ts
@@ -4,13 +4,7 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			allow: '/',
-			disallow: [
-				'/_next/static/media/',
-				'*/opengraph-image',
-				'*/twitter-image',
-				'*/icon',
-				'*/apple-icon'
-			],
+			disallow: ['/_next/static/media/', '*/icon', '*/apple-icon'],
 			userAgent: '*'
 		},
 		sitemap: 'https://rubriclabs.com/sitemap.xml'
