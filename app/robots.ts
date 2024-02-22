1ae328fd32768a44d7213037263f48b809e6bb7a
DexterStorey <dexter@dexterstorey.com>
2024-02-21 23:54:26 -0500

    allow scrape og

--- app/robots.ts
+++ app/robots.ts
@@ -4,12 +4,7 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			allow: '/',
-			disallow: [
-				'/_next/static/media/',
-				'*/opengraph-image',
-				'*/icon',
-				'*/apple-icon'
-			],
+			disallow: ['/_next/static/media/', '*/icon', '*/apple-icon'],
 			userAgent: '*'
 		},
 		sitemap: 'https://rubriclabs.com/sitemap.xml'
