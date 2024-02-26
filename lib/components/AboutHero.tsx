833ebe20464d81e1f299fb6074f79d0a1e8b27b6
Sarim Malik <sarim@rubriclabs.com>
2024-02-26 18:17:37 -0500

    Remove h1 on game of life for simplicity

--- lib/components/AboutHero.tsx
+++ lib/components/AboutHero.tsx
@@ -1,5 +1,4 @@
 'use client'
-import {AnimatePresence, motion} from 'framer-motion'
 import {useRouter} from 'next/navigation'
 import {useEffect, useState} from 'react'
 import Game from './game'
@@ -14,26 +13,10 @@ export default function Hero() {
 
 	return (
 		<div className='flex min-h-screen flex-col items-center justify-center gap-5 p-8'>
-			<div className={''}>
-				<Game
-					running={start}
-					setRunning={setStart}
-				/>
-			</div>
-			<AnimatePresence>
-				{start && (
-					<motion.div
-						animate={{opacity: 1}}
-						className='flex w-full flex-col items-center gap-16'
-						exit={{opacity: 0}}
-						initial={{opacity: 0}}
-						transition={{delay: 1, duration: 3}}>
-						<p className='max-w-2xl text-center text-4xl text-black sm:text-4xl dark:text-white'>
-							We&apos;re Rubric. A nomadic team of engineers and artists.
-						</p>
-					</motion.div>
-				)}
-			</AnimatePresence>
+			<Game
+				running={start}
+				setRunning={setStart}
+			/>
 		</div>
 	)
 }
