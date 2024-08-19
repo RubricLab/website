934155c91bcdbf0691c3c9c17350ce2c5871f2ec
JOYBOY-0 <falamino00@gmail.com>
2024-08-19 11:24:42 -0300

    upd

--- common/lab-blog-layout/background-grid.tsx
+++ common/lab-blog-layout/background-grid.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import {useLoaded} from '@/hooks/use-loaded'
 import {useGSAP} from '@gsap/react'
 import gsap from 'gsap'
-import {CSSProperties, useRef} from 'react'
+import {CSSProperties, useEffect, useRef} from 'react'
 
 const backgroundGradient = `repeating-linear-gradient(
 								to bottom,
@@ -17,22 +18,29 @@ export type BackgroundGridProps = {
 const BackgroundGrid = ({highlightColumns}: BackgroundGridProps) => {
   const tl = useRef<gsap.core.Timeline>(
     gsap.timeline({
-      paused: true,
-      onComplete: () => {
-        setTimeout(() => {
-          tl.current.reverse()
-        }, 1000)
-      }
+      paused: true
     })
   )
+
+  const loaded = useLoaded()
 
   useGSAP(() => {
     const columns = gsap.utils.toArray(
       document.querySelectorAll('[data-grid-col]')
     )
 
-    console.log(columns)
+    tl.current.to(columns, {
+      '--clip-progress': 1,
+      duration: 1,
+      stagger: 0.1,
+      ease: 'power3.out'
+    })
   })
+
+  useEffect(() => {
+    if (loaded) tl.current.play()
+  }, [loaded])
+
   return (
     <div className='pointer-events-none fixed inset-0 z-0 flex px-sides text-border'>
       <div
@@ -43,7 +51,8 @@ const BackgroundGrid = ({highlightColumns}: BackgroundGridProps) => {
             backgroundSize: '1px 1.5em',
             background: backgroundGradient,
             '--clip-progress': 0,
-            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)'
+            clipPath:
+              'polygon(0% 0%, 100% 0%, 100% calc(var(--clip-progress) * 100%), 0% calc(var(--clip-progress) * 100%))'
           } as CSSProperties
         }
       />
@@ -52,18 +61,21 @@ const BackgroundGrid = ({highlightColumns}: BackgroundGridProps) => {
           <div
             key={index}
             data-grid-col={index + 1}
+            style={
+              {
+                '--clip-progress': 0,
+                clipPath:
+                  'polygon(0% 0%, 100% 0%, 100% calc(var(--clip-progress) * 100%), 0% calc(var(--clip-progress) * 100%))'
+              } as CSSProperties
+            }
             className={`grid
 			${highlightColumns?.includes(index) ? 'bg-surface-contrast/[0.025]' : ''}`}>
             <span
               className='w-px justify-self-end'
-              style={
-                {
-                  backgroundSize: '1px 1.5em',
-                  background: backgroundGradient,
-                  '--clip-progress': 0,
-                  clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)'
-                } as CSSProperties
-              }
+              style={{
+                backgroundSize: '1px 1.5em',
+                background: backgroundGradient
+              }}
             />
           </div>
         ))}
