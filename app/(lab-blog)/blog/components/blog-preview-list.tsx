2fb5dba7439e454d621d0f7bf350609c21647023
Matias Perez <matiasperezpc@hotmail.com>
2024-08-21 04:06:48 -0300

    upd

--- app/(lab-blog)/blog/components/blog-preview-list.tsx
+++ app/(lab-blog)/blog/components/blog-preview-list.tsx
@@ -1,22 +1,73 @@
 'use client'
 
+import {BlogPostCard} from '@/lib/basehub/fragments/blog'
 import {useEffect, useRef} from 'react'
 import {BlogPostPreview} from './blogpost-preview'
 
-export const BlogPreviewList = ({posts}) => {
-  const listRef = useRef<HTMLDivElement>(null)
+interface BlogPreviewList {
+	posts: BlogPostCard[]
+}
 
-  useEffect(() => {}, [])
+export const BlogPreviewList = ({posts}: BlogPreviewList) => {
+	const listRef = useRef<HTMLDivElement>(null)
 
-  return (
-    <div
-      className='fixed right-sides top-header flex h-fold w-col-6 items-center justify-center [perspective:2000px]'
-      ref={listRef}>
-      {posts.map(p => (
-        <div className='absolute'>
-          <BlogPostPreview {...p} />
-        </div>
-      ))}
-    </div>
-  )
+	useEffect(() => {
+		if (!listRef.current) return
+
+		const listElm = listRef.current
+		const childElements = Array.from(listRef.current.children) as HTMLDivElement[]
+
+		const radius = 600 // adjust the radius as needed
+		const angleIncrement = (2 * Math.PI) / childElements.length
+
+		let ticker = 0
+
+		const animateElements = () => {
+			childElements.forEach((child, index) => {
+				const angle = index * angleIncrement
+				const y = radius * Math.cos(angle + ticker) + radius
+				const z = radius * Math.sin(angle + ticker) // translate center of circle -radius on z
+				const x = 0
+
+				// Calculate the rotation angle to match the tangent of the imaginary circle
+				const rotationAngle = angle + ticker - Math.PI
+
+				child.style.transform = `translate3d(${x}px, ${y}px, ${z}px) rotateX(${rotationAngle}rad)`
+				child.style.zIndex = Math.round(z).toString() // set z-index based on z coordinate
+			})
+
+			requestAnimationFrame(animateElements)
+		}
+
+		animateElements()
+
+		const handleWheel = (event: WheelEvent) => {
+			ticker -= event.deltaY * 0.001 // adjust the speed of animation based on wheel delta
+
+			event.preventDefault()
+		}
+
+		listRef.current.addEventListener('wheel', handleWheel)
+
+		return () => {
+			listElm?.removeEventListener('wheel', handleWheel)
+		}
+	}, [])
+
+	return (
+		<div
+			className='fixed right-sides top-header flex h-fold w-col-6 items-center justify-center [perspective:2000px]'
+			ref={listRef}>
+			{posts.map(p => (
+				<div
+					style={{
+						transformStyle: 'preserve-3d'
+					}}
+					className='absolute'
+					key={p._id}>
+					<BlogPostPreview {...p} />
+				</div>
+			))}
+		</div>
+	)
 }
