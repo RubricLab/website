df4eb0f710afa1a6a7f1a2085671d9550203d75c
JOYBOY-0 <falamino00@gmail.com>
2024-08-23 16:25:21 -0300

    fix curl

--- app/(lab-blog)/blog/components/preview-store.ts
+++ app/(lab-blog)/blog/components/preview-store.ts
@@ -1,5 +1,5 @@
-import { BlogPostCard } from '@/lib/basehub/fragments/blog'
-import { create } from 'zustand'
+import {BlogPostCard} from '@/lib/basehub/fragments/blog'
+import {create} from 'zustand'
 
 interface PreviewStore {
   selectedPost: BlogPostCard
@@ -9,10 +9,10 @@ interface PreviewStore {
 
 export const usePreviewStore = create<PreviewStore>((set, get) => ({
   selectedPost: null,
-  setSelectedPost: post => set({ selectedPost: post }),
-  clearSelectedPost: (post) => {
+  setSelectedPost: post => set({selectedPost: post}),
+  clearSelectedPost: post => {
     if (get().selectedPost._id === post._id) {
-      set({ selectedPost: null })
+      set({selectedPost: null})
     }
   }
 }))
