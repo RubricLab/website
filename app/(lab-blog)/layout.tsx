6448e6509e8a402e419a5f8f701530b12a78eacc
Matias Perez <matiasperezpc@hotmail.com>
2024-08-24 21:21:21 -0300

    add tab-focus-within

--- app/(lab-blog)/layout.tsx
+++ app/(lab-blog)/layout.tsx
@@ -5,32 +5,34 @@ import PlausibleProvider from 'next-plausible'
 import {Providers} from './providers'
 
 import Header from '@/common/lab-blog-layout/header'
-import { Leva } from '@/common/lab-blog-layout/leva'
+import {Leva} from '@/common/lab-blog-layout/leva'
 import {GsapSetup} from '@/lib/gsap'
+import {AppHooks} from './app-hooks'
 import './styles.css'
 
 export const metadata = {
-  metadataBase: new URL(META.siteURL)
+	metadataBase: new URL(META.siteURL)
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  return (
-    <html
-      lang='en'
-      suppressHydrationWarning
-      className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <head>
-        <PlausibleProvider domain='rubriclabs.com' />
-      </head>
+	return (
+		<html
+			lang='en'
+			suppressHydrationWarning
+			className={`${GeistSans.variable} ${GeistMono.variable}`}>
+			<head>
+				<PlausibleProvider domain='rubriclabs.com' />
+			</head>
 
-      <body className='px-sides font-mono'>
-        <GsapSetup />
-        <Providers>
-          <Header />
-          <main className='pt-header'>{children}</main>
-          <Leva />
-        </Providers>
-      </body>
-    </html>
-  )
+			<body className='px-sides font-mono'>
+				<GsapSetup />
+				<Providers>
+					<AppHooks />
+					<Header />
+					<main className='pt-header'>{children}</main>
+					<Leva />
+				</Providers>
+			</body>
+		</html>
+	)
 }
