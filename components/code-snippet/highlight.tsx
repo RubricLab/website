53c7ef3ec61ad64455f5448534237273abdb1411
tedspare <ted.spare@gmail.com>
2024-09-23 13:34:47 -0400

    Remove unused directive

--- components/code-snippet/highlight.tsx
+++ components/code-snippet/highlight.tsx
@@ -1,8 +1,8 @@
 import {
-  transformerNotationDiff,
-  transformerNotationErrorLevel,
-  transformerNotationHighlight,
-  transformerNotationWordHighlight
+	transformerNotationDiff,
+	transformerNotationErrorLevel,
+	transformerNotationHighlight,
+	transformerNotationWordHighlight
 } from '@shikijs/transformers'
 import {toJsxRuntime, type Components} from 'hast-util-to-jsx-runtime'
 import * as prod from 'react/jsx-runtime'
@@ -11,59 +11,58 @@ import {BundledTheme, codeToHast, type BundledLanguage} from 'shiki'
 type ComponentsToOverride = Pick<Components, 'pre' | 'code' | 'span'>
 
 type LightDarkTheme = {
-  light: BundledTheme
-  dark: BundledTheme
+	light: BundledTheme
+	dark: BundledTheme
 }
 
 // @ts-expect-error - `toJsxRuntime` is not typed
 const production = {Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs}
 
 const themeConfig: LightDarkTheme = {
-  light: 'catppuccin-latte',
-  dark: 'tokyo-night'
+	light: 'catppuccin-latte',
+	dark: 'tokyo-night'
 }
 
 export interface HighlighterProps {
-  children: string
-  lang: BundledLanguage
-  components?: Partial<ComponentsToOverride>
+	children: string
+	lang: BundledLanguage
+	components?: Partial<ComponentsToOverride>
 }
 
 export const Highlighter = async ({
-  children,
-  lang,
-  components
+	children,
+	lang,
+	components
 }: HighlighterProps) => {
-  const hast = await codeToHast(children, {
-    lang,
-    themes: themeConfig,
+	const hast = await codeToHast(children, {
+		lang,
+		themes: themeConfig,
 
-    transformers: [
-      transformerNotationDiff(),
-      transformerNotationErrorLevel(),
-      transformerNotationHighlight(),
-      transformerNotationWordHighlight(),
-      {
-        line(node, line) {
-          node.children = [
-            {
-              type: 'element',
-              tagName: 'span',
-              properties: {class: 'line-indicator'},
-              children: [{type: 'text', value: line.toString()}]
-            },
-            ...node.children
-          ]
-        }
-      }
-    ]
-  })
+		transformers: [
+			transformerNotationDiff(),
+			transformerNotationErrorLevel(),
+			transformerNotationHighlight(),
+			transformerNotationWordHighlight(),
+			{
+				line(node, line) {
+					node.children = [
+						{
+							type: 'element',
+							tagName: 'span',
+							properties: {class: 'line-indicator'},
+							children: [{type: 'text', value: line.toString()}]
+						},
+						...node.children
+					]
+				}
+			}
+		]
+	})
 
-  // @ts-expect-error - `toJsxRuntime` is not typed
-  const content = toJsxRuntime(hast, {
-    ...production,
-    components
-  })
+	const content = toJsxRuntime(hast, {
+		...production,
+		components
+	})
 
-  return content
+	return content
 }
