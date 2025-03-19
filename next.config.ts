e47cc73774a736804e0fe654ac7f411d3a7a3243
tedspare <ted.spare@gmail.com>
2025-03-19 11:49:48 -0400

    Add code syntax highlighting

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
 import createMDX from '@next/mdx'
 import type { NextConfig } from 'next'
+import rehypePrettyCode from 'rehype-pretty-code'
+
+const rehypeOptions = {
+	keepBackground: false,
+	theme: {
+		// To view themes: shiki.style/themes#themes
+		dark: 'github-dark-dimmed',
+		light: 'github-light'
+	}
+}
 
 const nextConfig = {
 	reactStrictMode: true,
-	pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
-	experimental: {
-		mdxRs: true
-	},
-	images: {
-		remotePatterns: [
-			{
-				hostname: 'assets.basehub.com'
-			},
-			{
-				hostname: 'localhost'
-			}
-		]
-	}
+	pageExtensions: ['md', 'mdx', 'ts', 'tsx']
 } satisfies NextConfig
 
 const withMDX = createMDX({
-	// Add markdown plugins here, as desired
+	extension: /\.mdx?$/,
+	options: {
+		rehypePlugins: [[rehypePrettyCode, rehypeOptions]]
+	}
 })
 
 export default withMDX(nextConfig)
