12d96b542c471f2e70d65152bfd17787e45caaaa
tedspare <ted.spare@gmail.com>
2025-03-25 11:28:57 -0400

    Fix contact form layout shift

--- src/app/(rest)/contact/contact-form.tsx
+++ src/app/(rest)/contact/contact-form.tsx
@@ -16,17 +16,13 @@ export const ContactForm = () => {
 			{({ pending, state }) => (
 				<>
 					<div className="flex min-h-56 w-full flex-col items-center justify-center text-center">
-						{state?.success ? (
-							<p>Thanks. We'll be in touch soon.</p>
-						) : (
-							<div className="flex w-full flex-col gap-2">
-								{/* biome-ignore lint/a11y/noAutofocus: <explanation> */}
-								<input autoFocus placeholder="Name" name="name" required />
-								<input placeholder="Email" name="email" required />
-								<input placeholder="Company" name="company" required />
-								<textarea placeholder="Message" name="message" required className="resize-none" />
-							</div>
-						)}
+						<div className="flex w-full flex-col gap-2">
+							{/* biome-ignore lint/a11y/noAutofocus: <explanation> */}
+							<input autoFocus placeholder="Name" name="name" required />
+							<input placeholder="Email" name="email" required />
+							<input placeholder="Company" name="company" required />
+							<textarea placeholder="Message" name="message" required className="resize-none" />
+						</div>
 					</div>
 					<Button type="submit" disabled={pending || !!state?.success} className="w-full">
 						{pending ? 'Submitting...' : state?.success ? 'Submitted' : 'Submit'}
@@ -35,6 +31,7 @@ export const ContactForm = () => {
 							<kbd>⏎</kbd>
 						</div>
 					</Button>
+					{state?.success ? <p>Thanks. We'll be in touch soon.</p> : null}
 					<p className="h-5 text-danger text-sm">{state?.error || ''}</p>
 				</>
 			)}
