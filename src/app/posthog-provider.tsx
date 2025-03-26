8bb0fca359731c5de32ac256cb0eb9f95abaf070
tedspare <ted.spare@gmail.com>
2025-03-26 11:49:58 -0400

    Make Posthog env vars safe

--- src/app/posthog-provider.tsx
+++ src/app/posthog-provider.tsx
@@ -5,11 +5,12 @@ import posthog from 'posthog-js'
 import { usePostHog } from 'posthog-js/react'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import { Suspense, useEffect } from 'react'
+import { env } from '~/lib/env'
 
 export const PostHogProvider = ({ children }: { children: React.ReactNode }) => {
 	useEffect(() => {
-		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+		posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+			api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
 			person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
 			capture_pageview: false // Disable automatic pageview capture, as we capture manually
 		})
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,12 +2,17 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import z from 'zod'
 
 export const env = createEnv({
-	client: {},
+	client: {
+		NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
+		NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1)
+	},
 	runtimeEnv: {
 		ROS_API_URL: process.env.ROS_API_URL,
 		ROS_SECRET: process.env.ROS_SECRET,
 		VERCEL_URL: process.env.VERCEL_URL,
-		NODE_ENV: process.env.NODE_ENV
+		NODE_ENV: process.env.NODE_ENV,
+		NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+		NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST
 	},
 	server: {
 		ROS_API_URL: z.string().min(1),
