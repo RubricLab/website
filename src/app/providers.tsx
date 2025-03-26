e5939873ae1508b47be0c5c5b667735b1fcd9f04
Sarim Malik <sarimrasheed@gmail.com>
2025-03-26 01:20:40 -0400

    Add posthog event tracking

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,13 @@
 
 import type { ReactNode } from 'react'
 import { Toaster } from 'sonner'
+import { PostHogProvider } from './posthog-provider'
 
 export default function Providers({ children }: { children: ReactNode }) {
 	return (
-		<>
+		<PostHogProvider>
 			<Toaster position="bottom-right" />
 			{children}
-		</>
+		</PostHogProvider>
 	)
 }
