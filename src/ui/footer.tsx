045492737990fc1be791ac5945a3e261f460894a
Sarim Malik <sarimrasheed@gmail.com>
2025-03-27 00:55:53 +0400

    Add missing work link to footer. Set fixed date to privacy policy updates and move it up.

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -28,6 +28,25 @@ const socials = [
 	}
 ]
 
+const links = [
+	{
+		label: 'Newsletter',
+		href: '/newsletter'
+	},
+	{
+		label: 'Work',
+		href: '/work'
+	},
+	{
+		label: 'Brand',
+		href: 'https://brand.rubriclabs.com'
+	},
+	{
+		label: 'Privacy',
+		href: '/privacy'
+	}
+]
+
 export const Footer = ({ className }: { className?: string }) => {
 	const posthog = usePostHog()
 
@@ -62,9 +81,11 @@ export const Footer = ({ className }: { className?: string }) => {
 					<div className="flex w-fit flex-col gap-4">
 						<p>Links</p>
 						<div className="flex flex-wrap gap-5 text-secondary">
-							<Link href="/newsletter">Newsletter</Link>
-							<Link href="https://brand.rubriclabs.com">Brand</Link>
-							<Link href="/privacy">Privacy</Link>
+							{links.map(({ label, href }) => (
+								<Link key={label} href={href}>
+									{label}
+								</Link>
+							))}
 						</div>
 					</div>
 				</div>
