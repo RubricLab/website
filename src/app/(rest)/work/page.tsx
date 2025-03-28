e643fe2bb6d6093b3df62634ab2316878f871d09
tedspare <ted.spare@gmail.com>
2025-03-28 18:32:22 -0400

    Add CTA to work page

--- src/app/(rest)/work/page.tsx
+++ src/app/(rest)/work/page.tsx
@@ -1,9 +1,11 @@
+import { CTA } from '~/ui/cta'
 import { WorkTable } from '~/ui/work-table'
 
 export default function Page() {
 	return (
 		<div className="flex flex-col items-center gap-16 p-4 py-32">
 			<WorkTable />
+			<CTA />
 		</div>
 	)
 }
