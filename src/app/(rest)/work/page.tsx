0021419d4333509e065fcc0d3a44606ad78f79b4
Sarim Malik <sarimrasheed@gmail.com>
2025-03-31 12:15:32 +0500

    Remove links from highlighted text in testimonials

--- src/app/(rest)/work/page.tsx
+++ src/app/(rest)/work/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 			<div className="flex max-w-4xl flex-col items-center gap-16">
 				<div className="flex flex-col items-center gap-2">
 					<h1>Work</h1>
-					<p className="text-secondary">Past projects and technical writing</p>
+					<p className="text-secondary">Our past projects and technical writing</p>
 				</div>
 				<WorkTable />
 				<CTA />
--- src/ui/testimonials.tsx
+++ src/ui/testimonials.tsx
@@ -9,9 +9,7 @@ export const Testimonials = () => {
 						"Rubric was an absolute pleasure to work with. They were available to meet on short notice and
 						displayed an immense desire to meet our near-impossible deadlines. Their knowledge of
 					</span>{' '}
-					<Link href="/work#Sligo" className="text-primary">
-						complex AI solutions
-					</Link>
+					complex AI solutions
 					<span className="text-secondary/50">
 						{' '}
 						is impressive. I will definitely be working with the Rubric team again soon."
@@ -28,9 +26,7 @@ export const Testimonials = () => {
 						"In just a few weeks, Rubric went from initial concepts to delivering an engaging AI video
 						experience that reached{' '}
 					</span>
-					<Link href="/work#Graphite" className="text-primary">
-						thousands of users
-					</Link>
+					thousands of users
 					<span className="text-secondary/50">
 						. They&apos;re fluent in novel technologies, creative, highly responsive, and went the extra
 						mile to follow through and iterate with us even after initial handoff."
@@ -47,9 +43,7 @@ export const Testimonials = () => {
 						"Working with Rubric has been like having a CTO in our back pocket. They pair the best in
 						strategy with a killer product team to consistently deliver{' '}
 					</span>
-					<Link href="/work#Weave" className="text-primary">
-						on time, every time
-					</Link>
+					on time, every time
 					<span className="text-secondary/50">
 						. From our first conversation when they took time to understand our business needs, I knew
 						trusting them with our MVP build and every iteration since was the right choice."
