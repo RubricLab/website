6b063dd484dea1aa300cd2cb2a64ca8592dc7276
tedspare <ted.spare@gmail.com>
2025-11-03 12:16:48 -0500

    Remove logo title tag

--- src/ui/logos/rubric.tsx
+++ src/ui/logos/rubric.tsx
@@ -6,6 +6,7 @@ export const Rubric = ({
 	style?: React.CSSProperties
 }) => {
 	return (
+		// biome-ignore lint/a11y/noSvgWithoutTitle: title is visible
 		<svg
 			xmlns="https://www.w3.org/2000/svg"
 			viewBox="0 0 99 99"
@@ -13,7 +14,6 @@ export const Rubric = ({
 			shapeRendering="crispEdges"
 			style={style}
 		>
-			<title>Rubric Logo</title>
 			<rect x="0" y="0" width="33" height="33" fill="currentColor" />
 			<rect x="66" y="0" width="33" height="33" fill="currentColor" />
 			<rect x="0" y="33" width="66" height="33" fill="currentColor" />
