9ce096e33d982ce76f0093502101d2d5d3fdf98e
Sarim Malik <sarimrasheed@gmail.com>
2025-11-06 13:41:14 -0500

    Remove page-title class and only modify article h1, h2, h3 instead

--- src/app/(rest)/newsletter/page.tsx
+++ src/app/(rest)/newsletter/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
 	return (
 		<div className="flex min-h-screen flex-col items-center gap-16 p-4 py-32">
 			<div className="flex flex-col items-center gap-4 text-center">
-				<h1 className="page-title">The Grid</h1>
+				<h1>The Grid</h1>
 				<p className="text-secondary">3 actionable insights. Once a week. Straight to your inbox.</p>
 				<NewsletterForm className="w-full" />
 			</div>
