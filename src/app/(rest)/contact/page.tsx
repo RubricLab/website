89fe4ef48ce6f8fb16ce17cc1e45376c84f4067b
Sarim Malik <sarimrasheed@gmail.com>
2025-11-06 13:29:48 -0500

    Size down blog h1 for better readability

--- src/app/(rest)/contact/page.tsx
+++ src/app/(rest)/contact/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 		<div className="flex min-h-screen max-w-full flex-col items-center justify-center gap-16 p-4 py-32">
 			<div className="flex w-full max-w-lg flex-col items-center gap-8">
 				<div className="flex flex-col items-center gap-2">
-					<h1>Contact</h1>
+					<h1 className="page-title">Contact</h1>
 					<p className="text-secondary">Get in touch with us</p>
 				</div>
 				<ContactForm />
--- src/app/(rest)/work/page.tsx
+++ src/app/(rest)/work/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
 		<div className="flex flex-col items-center p-4 py-32">
 			<div className="flex max-w-4xl flex-col items-center gap-16">
 				<div className="flex flex-col items-center gap-2">
-					<h1>Work</h1>
+					<h1 className="page-title">Work</h1>
 					<p className="text-secondary">Our past projects and technical writing</p>
 				</div>
 				<WorkTable />
